add61db3ba45ef28ab53572a87576b6b1e6cf838
이지은 <ee2e@naver.com>
2020-07-23 22:22:41 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import React from "react";
-
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
@@ -21,30 +13,55 @@ const foodILike = [
     name: "Samgyeopsal",
     image:
       "http://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
       "https://img.huffingtonpost.com/asset/5727ef362200003600255394.jpeg?cache=XowoERt6W4&ops=1910_1000",
+    rating: 4.8,
   },
   {
     id: 4,
     name: "Doncasu",
     image: "p-content/uploads/2017/07/donkasu-036.jpg?x61413",
+    rating: 5.5,
   },
   {
     id: 5,
     name: "kimbap",
     image: "https://i.ytimg.com/vi/Y-Y9CXGRJPU/maxresdefault.jpg",
+    rating: 4.9,
   },
 ];
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
