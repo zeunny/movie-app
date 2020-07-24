3eb1575808d8e22fd2f70466dbeddd0b7d620d51
이지은 <ee2e@naver.com>
2020-07-24 20:12:47 +0900

    Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -3,15 +3,22 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // state 를 쓰지 않을 경우에는 class component 를 가지지 않아도 됨
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div class="movies__movie">
+    <div className="movies__movie">
       {/* title 을 지정하면 그림에 마우스를 올렸을 때 title 이 나옴 */}
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie_title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key="index" className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -23,6 +30,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
