5295545cb9043b73eac3342c2fcb5b1a6088c5a4
이지은 <ee2e@naver.com>
2020-07-24 21:04:18 +0900

    Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -13,12 +13,12 @@ function Movie({ year, title, summary, poster, genres }) {
         <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">
           {genres.map((genre, index) => (
-            <li key="index" className="genres__genre">
+            <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
