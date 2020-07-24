6b5728d5321d3a420d0526ec034e71b68015d3be
이지은 <ee2e@naver.com>
2020-07-24 23:21:41 +0900

    Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 // npm install react-router-dom
 function App() {
   return (
-    <HashRouter>
+    // github pages에 에 올릴 때는 HashBrowser를 사용하는 것이 좋음
+    // '#'을 없애고 싶으면 BrowserRouter를 사용하면 됨
+    <BrowserRouter>
+      <Navigation />
       {/* exact 는 이 path가 아니면 렌더링 하지 않는다는 뜻
       (없으면 /about 에 가면 Home이랑 About이 둘 다 렌더링 됨) */}
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
