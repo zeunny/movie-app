0c3053a9977a1c41825a5041f1a75e1719514c68
이지은 <ee2e@naver.com>
2020-07-24 22:30:29 +0900

    Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
+// npm install react-router-dom
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      {/* exact 는 이 path가 아니면 렌더링 하지 않는다는 뜻
+      (없으면 /about 에 가면 Home이랑 About이 둘 다 렌더링 됨) */}
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
