c43bbe13c3872c3609af781ab3f6788a6341731f
이지은 <ee2e@naver.com>
2020-07-24 20:52:48 +0900

    Styles

--- src/Movie.js
+++ src/Movie.js
@@ -5,13 +5,13 @@ import "./Movie.css";
 // state 를 쓰지 않을 경우에는 class component 를 가지지 않아도 됨
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
+    <div className="movie">
       {/* title 을 지정하면 그림에 마우스를 올렸을 때 title 이 나옴 */}
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
-        <h3 className="movie_title">{title}</h3>
+        <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key="index" className="genres__genre">
               {genre}
